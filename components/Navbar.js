e66d11e066fa19828859be6d3fb74942c051a2f4
Gabriel  Barreto Otero <120350521+Luxx12@users.noreply.github.com>
2026-01-29 21:58:14 -0500

    Enhance Navbar.js with descriptive comments
    Add comments to Navbar component for clarity

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,9 @@
 'use client'
 
+// recieve props from page.js in order to know which pages to highlight, along with setCurrentView which allows pages to change using the Nav Bar
 export default function Navbar({ currentView, setCurrentView }) {
+  
+  // sets up buttons array with id's equivalent to what it corresponds to in page.js along with a label of it to display it, allows us to easily loop over each button instead of manually creating it
   const navItems = [
     { id: 'home', label: 'Home' },
     { id: 'about', label: 'About' },
@@ -10,6 +13,7 @@ export default function Navbar({ currentView, setCurrentView }) {
     { id: 'game', label: 'Play' },
   ]
 
+  // changes page to wtv the id of the button was that was pressed, gives time for react to render
   const handleNav = (id) => {
     setCurrentView(id)
     setTimeout(() => {
